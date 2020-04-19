f63450a9494362058803a19e051f5f3f11cc7f3d
Jessica K <jessikim807@gmail.com>
2020-04-19 15:16:57 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,22 +1,39 @@
 require_relative './part_1_solution.rb'
 
-# def apply_coupons(cart, coupons)
-#   coupons.each do |coupon|
-#     coupon.each do |attribute, value|
-#       name = coupon[:item]
-#       if cart[name] && cart[name][:count] >= coupon[:num]
-#         if cart ["#{name} W/COUPON"]
-#           cart["#{name} W/COUPON"][:count] += 1
-#         else
-#           cart["#{name} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count => 1}
-#         end
-#         cart[name][:count] -= coupon[:num]
-#       end
-#     end
-#   end
-#   cart
-# end
+def apply_coupons(cart, coupons)
+  coupons.each do |coupon|
+    coupon.each do |attribute, value|
+      name = coupon[:item]
+      binding.pry
+      if cart[name] && cart[name][:count] >= coupon[:num]
+        if cart ["#{name} W/COUPON"]
+          cart["#{name} W/COUPON"][:count] += 1
+        else
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count => 1}
+        end
+        cart[name][:count] -= coupon[:num]
+      end
+    end
+  end
+  cart
+end
 
+def apply_coupons(cart, coupon)
+  coupons.each do |item|
+    name_of_item = item[:item]
+    if cart.include?(name_of_item) == true && cart[name_of_item][:count] >= item[:num]
+      cart[name_of_item][:count] = cart[name_of_item][:count] - item[:num]
+      new_cart = name_of_item + ("W/COUPON")
+      puts cart.include?(new_item)
+      if cart.include?(new_item) == false
+        cart[new_item] = {:price => item[:cost], :clearance => cart[name_of_item][:clearance], :count => 1}
+      else
+        cart[new_item][:count] += 1
+      end
+    end
+  end
+  cart
+end
 
 # def apply_coupons(cart, coupons)
 #   #return cart if coupons == []
@@ -52,22 +69,7 @@ require_relative './part_1_solution.rb'
 #   return cart
 # end
 
-def apply_coupons(cart, coupon)
-  coupons.each do |item|
-    name_of_item = item[:item]
-    if cart.include?(name_of_item) == true && cart[name_of_item][:count] >= item[:num]
-      cart[name_of_item][:count] = cart[name_of_item][:count] - item[:num]
-      new_cart = name_of_item + ("W/COUPON")
-      puts cart.include?(new_item)
-      if cart.include?(new_item) == false
-        cart[new_item] = {:price => item[:cost], :clearance => cart[name_of_item][:clearance], :count => 1}
-      else
-        cart[new_item][:count] += 1
-      end
-    end
-  end
-  cart
-end
+
 
   # Consult README for inputs and outputs
   #
